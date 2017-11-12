d42be547734135797be3a32df01be6941a5d5f5d
igromov <nocreative79@gmail.com>
2017-11-12 20:13:30 +0300

    Default

---
+++ load.js
@@ -0,0 +1,9 @@
+
+		<script>
+		  $(window).on('load', function () {
+		    $preloader = $('.loaderArea'),
+		      $loader = $preloader.find('.container_pr');
+		    $loader.fadeOut();
+		    $preloader.delay(350).fadeOut('slow');
+		  });
+		</script>
